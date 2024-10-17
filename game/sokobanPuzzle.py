ba8ca87309e13bdbf495e500897048329b4b00c8
Merouane <merouane.mezouari@etu.usthb.dz>
2024-10-17 14:29:24 -0700

    adding grid logic

--- game/sokobanPuzzle.py
+++ game/sokobanPuzzle.py
@@ -1,41 +1,70 @@
 class SokobanPuzzle:
-  def __init__(self, level_file):
+    def __init__(self, level_file):
       self.width = 0
       self.height = 0
       self.obstacles = set()
       self.goals = set()
       self.boxes = set()
       self.player = None
+      self.grid = []
       self.load_level(level_file)
 
-  def load_level(self, level_file):
-      with open(level_file, 'r') as f:
-          lines = f.readlines()
+    def load_level(self, level_file):
+        with open(level_file, 'r') as f:
+            lines = f.readlines()
 
-      self.height = len(lines)
-      self.width = max(len(line.rstrip('\n')) for line in lines)
+        self.height = len(lines)
+        self.width = max(len(line.rstrip('\n')) for line in lines)
 
-      for y, line in enumerate(lines):
-          for x, char in enumerate(line.rstrip('\n')):
-              pos = (x, y)
-              if char == 'O':
-                  self.obstacles.add(pos)
-              elif char == ' ':
-                  pass  
-              elif char == 'S':
-                  self.goals.add(pos)
-              elif char == 'B':
-                  self.boxes.add(pos)
-              elif char == '*':
-                  self.boxes.add(pos)
-                  self.goals.add(pos)
-              elif char == 'R':
-                  self.player = pos
-              elif char == '.':
-                  self.player = pos
-                  self.goals.add(pos)
-              else:
-                  pass  
+        self.grid = []  
+
+        for y, line in enumerate(lines):
+            row = []
+            line = line.rstrip('\n')
+            for x in range(self.width):
+                if x < len(line):
+                    char = line[x]
+                else:
+                    char = ' '  
+                pos = (x, y)
+
+                if char == 'O':
+                    self.obstacles.add(pos)
+                    row.append('O')
+                elif char == ' ':
+                    row.append(' ')
+                elif char == 'S':
+                    self.goals.add(pos)
+                    row.append('S')
+                elif char == 'B':
+                    self.boxes.add(pos)
+                    row.append('B')
+                elif char == '*':
+                    self.boxes.add(pos)
+                    self.goals.add(pos)
+                    row.append('*')
+                elif char == 'R':
+                    self.player = pos
+                    row.append('R')
+                elif char == '.':
+                    self.player = pos
+                    self.goals.add(pos)
+                    row.append('.')
+                else:
+                    row.append(' ')  
+
+            self.grid.append(row)
+              
+    def isGoal(self):
+        pass
+    
+    
+    def successorFunction(self):
+        pass 
+    
+    def deepCopy(self):
+        newState = SokobanPuzzle()
+        newState. = self.width 
               
               
 
